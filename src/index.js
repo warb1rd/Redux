30e4654e3a2326f832a07b5e2953499eb0b4f1df
Mahima Rao <mahima.m23@gmail.com>
2018-05-05 22:20:03 -0700

    api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import API_KEY from './config_keys.js';
+import SearchBar from './components/searchBar.js'
+//Component produces HTML 
+const App = function(){
+    return <div>Hi!</div>                                       //jsx gets transpiled to vanilla js
+}
 
+// Take the generated HTML and put in on the page in the DOM
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
